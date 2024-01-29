1b5bcb0524c373f0ce1ab475351e0d101bc73284
sergioandresco <programador2@interactiva.net.co>
2024-01-29 17:04:45 -0500

    Creation a model for a db and logic for the update a number of places available

--- apisReserve/admin.py
+++ apisReserve/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .models import (
+    User,
+    Events,
+    Reservs,
+    Event_comments,
+)
 
 # Register your models here.
+
+admin.site.register(Events)
+admin.site.register(Reservs)
+admin.site.register(Event_comments)
